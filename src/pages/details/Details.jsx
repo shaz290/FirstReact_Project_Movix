45156ccc0c0fe4d9a27fbc8cd239060bd3a7fad4
shaz.ahmed <steve.josh@kanrad.com>
2024-02-15 15:18:20 +0530

    Details

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 import DetailsBanner from './detailsBanner/DetailsBanner'
 import Cast from './cast/Cast';
 import VideosSection from './videosSection/VideosSection';
-import Similar from './carousels/similar';
+import Similar from "./carousels/Similar"
 import Recommendation from './carousels/Recommendation';
 
 
